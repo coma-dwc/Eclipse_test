6fe70e02ba9a13d7be1fc47c096ed8239319495e
coma-dwc <tamagomapon@gmail.com>
2023-07-22 01:49:52 +0900

    youso20 commit

--- src/youso20/youso20.java
+++ src/youso20/youso20.java
@@ -4,3 +4,32 @@ package youso20;
 //[0]～[10]の値の奇数のみカンマ区切りで、
 //[11]～[19]の値は偶数をカンマ区切りで表示させる
 
+public class youso20 {
+	public static void main (String[] args) {
+		int number[] = new int[20];
+		String str ="";
+		number[0] = 0;
+		for(int i = 1; i < number.length; i++) {
+			number[i] = number[i - 1] + 5;
+		}
+		
+		System.out.print("\n奇数 : ");
+		for(int i = 0; i < 11; i++) {
+			if(number[i] % 2 != 0) {
+				str += String.valueOf(number[i]) + ",";
+			}
+		}
+		
+		System.out.print(str.substring(0, str.length() - 1));
+		System.out.print("\n偶数 : ");
+		str = "";
+		for(int i = 11; i < 20; i++) {
+			if(number[i] % 2 == 0) {
+				str += String.valueOf(number[i]) + ",";
+			}
+		}
+		
+		System.out.print(str.substring(0, str.length() - 1));
+		System.out.print("\n");
+	}
+}
