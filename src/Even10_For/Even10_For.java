d2996d148da46f63a4d91ab5b59396e9071629c7
coma-dwc <tamagomapon@gmail.com>
2023-07-27 17:38:59 +0900

    降順の場合

--- src/Even10_For/Even10_For.java
+++ src/Even10_For/Even10_For.java
@@ -1,9 +1,23 @@
+//package Even10_For;
+//
+////1～10までの数字のうち、偶数のみを昇順で表示させる(for文)
+//public class Even10_For {
+//	public static void main( String[] args ) {
+//		for ( int i = 1; i <= 10; i++ ) {
+//			// 偶数の判定
+//			if ( 0 == ( i % 2 ) )
+//				System.out.println( i );
+//		}
+//	}
+//}
+
+
 package Even10_For;
 
-//1～10までの数字のうち、偶数のみを昇順で表示させる(for文)
+//1～10までの数字のうち、偶数のみを降順で表示させる(for文)
 public class Even10_For {
-	public static void main( String[] args ) {
-		for ( int i = 1; i <= 10; i++ ) {
+	public static void main(String[] args ) {
+		for (int i = 10; i >= 1; i--) {
 			// 偶数の判定
 			if ( 0 == ( i % 2 ) )
 				System.out.println( i );
